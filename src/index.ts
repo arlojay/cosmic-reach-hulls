349cefba645108d3f4b59acfc56ac75c09f049fd
Arlo Virta <35749102+arlojay@users.noreply.github.com>
2025-03-08 17:27:15 -0600

    Added cable paneling

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
-import { Mod, Writer } from "cosmic-reach-dag";
+import { Directions, LangKeyLanguage, loadBlockbenchModel, Mod, Writer } from "cosmic-reach-dag";
 
-const mod = new Mod("mymod"); // Also update in package.json!
+const mod = new Mod("hulls");
 const writer = new Writer(mod, true);
 
 main();
 
 async function main() {
-    // Write your mod here
+    const block = mod.createBlock("cable_paneling");
+    const baseModel = await loadBlockbenchModel(mod, "cable_paneling", "./assets/models/cable-paneling.bbmodel");
 
-    writer.write("./mod/"); // Change to your Cosmic Reach mods folder to automatically update it
+    block.createDefaultLangKey().addTranslation("Cable Paneling", LangKeyLanguage.en_us);
+    block.fallbackParams = {
+        lightAttenuation: 1,
+        isOpaque: false
+    };
+
+    for(let direction of Directions.nativeRotation) {
+        const state = block.createState({ direction: direction.name });
+        state.setBlockModel(baseModel);
+        state.placementRules = "default";
+
+        if(direction.z > 0) state.rotXZ = 0;
+        if(direction.x < 0) state.rotXZ = 90;
+        if(direction.z < 0) state.rotXZ = 180;
+        if(direction.x > 0) state.rotXZ = 270;
+    }
+
+    writer.write(process.env.LOCALAPPDATA + "/cosmic-reach/mods/");
 }
